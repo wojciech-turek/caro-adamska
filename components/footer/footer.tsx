f63b5a62147ba489394ef5b197580dda09758a13
boguslawdanek <boguslawdanek@gmail.com>
2024-03-19 13:29:37 +0100

    Added new component footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-salmon text-black py-73 text-center">
+      <div className="container mx-auto">
+        <div className="text-24">Caro Adamska</div>
+        <div className="text-24">karolina.adamska@gmail.com</div>
+        <div className="text-24">+48 510 593 718</div>
+        <div className="text-24">Icon Icon Icon</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
