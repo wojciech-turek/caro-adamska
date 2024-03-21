327ef4b880434313a7e21718b1621acbe0caa9c9
boguslawdanek <boguslawdanek@gmail.com>
2024-03-21 16:11:04 +0100

    homepage carousel component

---
+++ components/HomepageCarousel/HomepageCarousel.tsx
@@ -0,0 +1,52 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
+import style from "./homapageCarousel.module.scss";
+
+interface HomepageCarouselProps {
+  title: string;
+  url: string;
+}
+
+const HomepageCarousel = ({ title, url }: HomepageCarouselProps) => {
+  return (
+    <div className="container mx-auto">
+      <h3 className="text-4xl font-medium text-center mb-6">{title}</h3>
+
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full"
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <Link href={url}>
+                <div className="p-1">
+                  <Image
+                    className="rounded-lg"
+                    width={395}
+                    height={461}
+                    src={"/1.jpeg"}
+                    alt={"1"}
+                  />
+                </div>
+              </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className={style.carouselButton} />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default HomepageCarousel;
