e39f677a776a74270277e1408cd1c1dc6ec2af6e
boguslawdanek <boguslawdanek@gmail.com>
2024-03-21 16:36:33 +0100

    publication page placeholder

---
+++ app/publications/[publicationsId]/page.tsx
@@ -0,0 +1,7 @@
+export default function PublicationsDetails({ params }) {
+  return (
+    <div className="w-full text-center mb-114">
+      <h1>Publications details {params.publicationsId}</h1>
+    </div>
+  );
+}
---
+++ app/publications/page.tsx
@@ -0,0 +1,7 @@
+export default function Publications() {
+  return (
+    <div className="w-full text-center mb-114">
+      <h1>Publications</h1>
+    </div>
+  );
+}
