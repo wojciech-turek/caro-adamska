9be002f7ed661d7a5ab08f2a32f43c23eb1e72e7
boguslawdanek <boguslawdanek@gmail.com>
2024-03-21 16:32:51 +0100

    homepage in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+import HomepageCarousel from "@/components/HomepageCarousel/HomepageCarousel";
+import Contact from "@/components/contact/contact";
+import Introduction from "@/components/introduction/introduction";
+import Reviews from "@/components/reviews/reviews";
+
 export default function Home() {
-  return <div> hi</div>;
+  return (
+    <>
+      <Introduction />
+
+      <HomepageCarousel title={"Publications"} url={"/publications"} />
+
+      <HomepageCarousel
+        title={"Commercial collabs"}
+        url={"/commecial-collabs"}
+      />
+      <HomepageCarousel
+        title={"Personal projects"}
+        url={"/personal-projects"}
+      />
+
+      {/* <Reviews /> */}
+
+      <Contact />
+    </>
+  );
 }
