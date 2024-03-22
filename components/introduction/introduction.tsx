61e1dd879dfdffd72090cc8a051a826d43e7d232
boguslawdanek <boguslawdanek@gmail.com>
2024-03-22 01:37:19 +0100

    homepage - introduction component correct styling

--- components/introduction/introduction.tsx
+++ components/introduction/introduction.tsx
@@ -1,14 +1,15 @@
 const Introduction = () => {
   return (
-    <div className="w-full text-center mb-114">
-      <h1 className="text-4xl font-medium text-center">Caro Adamska</h1>
-
-      <h2>Stylist and Make-Up Artist</h2>
-      <p>
+    <div className="w-full text-center mb-68">
+      <h1 className="text-150 font-medium text-center">Caro Adamska</h1>
+      <h2 className="text-40 mb-30 font-medium text-center">
+        Stylist and Make-Up Artist
+      </h2>
+      <h2 className="text-40 font-medium text-center">
         Need a stylist for your next photoshoot? Looking for a fashion advice?
         Make up for some special occasion? Or maybe you want to rework your
-        personal style? Let’s get in touch - I will figure it out for you.
-      </p>
+        personal style? Let`s get in touch - I will figure it out for you.
+      </h2>
     </div>
   );
 };
