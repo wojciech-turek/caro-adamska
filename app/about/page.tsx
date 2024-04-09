2dc2eceab50255f771dd6a34afbf62086e3ff090
boguslawdanek <boguslawdanek@gmail.com>
2024-04-09 18:57:45 +0200

    about page mobile responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,24 +23,28 @@ export default async function About() {
   const data: AboutData = await getData();
 
   return (
-    <>
+    <div className="flex flex-col px-8">
       <div className="w-full text-center mb-114">
-        <h1 className="text-4xl mb-8 md:mb-24 font-medium">{data.title}</h1>
+        <h1 className="text-4xl mb-6 md:mb-12 lg:mb-24 font-medium">
+          {data.title}
+        </h1>
       </div>
-      <div className="lg:float-right lg:text-right lg:w-1/2 lg:pr-4 lg:mb-0 sm:text-center">
-        <Image
-          width={572}
-          height={564}
-          src={urlFor(data.image).url()}
-          alt={data.title}
-          className="float-right rounded-lg overflow-hidden"
-        />
-      </div>
-      <div className="w-full md:w-1/2 md:pl-4">
-        <div className="prose prose-p:text-lg prose-p:mb-4 prose-p:mt-0">
-          <PortableText value={data.content} />
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex lg:w-1/2">
+          <Image
+            width={572}
+            height={564}
+            src={urlFor(data.image).url()}
+            alt={data.title}
+            className="rounded-lg overflow-hidden mb-8 block mx-auto justify-center"
+          />
+        </div>
+        <div className="flex w-full lg:w-1/2 md:pl-4">
+          <div className="prose prose-p:text-lg prose-p:mb-4 prose-p:mt-0">
+            <PortableText value={data.content} />
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
