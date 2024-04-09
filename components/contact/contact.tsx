9e45f798ec60c26c2259c034542ab702ebcbd6b7
boguslawdanek <boguslawdanek@gmail.com>
2024-04-09 16:29:57 +0200

    homepage contact and contact form

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,32 +1,79 @@
 import Image from "next/image";
+import ContactForm from "../contactForm/ContactForm";
 
 const Contact = () => {
   return (
     <div className="w-full text-center mb-114">
-      <h2 className="text-3xl font-medium text-center mb-5">Contact</h2>
-      <p className="mb-10">
-        To contact me - use the below contact form or drop mi a direct message
-        via one of my personal channels.
-      </p>
-      <div className="flex flex-wrap">
-        <div className="w-full sm:w-1/3">
+      <div className="px-8 py-4 md:px-8 md:py-0">
+        <h2 className="text-3xl font-medium text-center mb-5">Contact</h2>
+        <p className="mb-4 md:mb-10 text-16">
+          To contact me - use the below contact form or drop mi a direct message
+          via one of my personal channels.
+        </p>
+      </div>
+      <div className="flex flex-wrap md:mt-16">
+        <div className="w-full sm:w-1/3 px-8 mb:px-0">
           <Image
-            width={572}
-            height={564}
+            width={300}
+            height={300}
             src={"/caro_adamska.jpeg"}
             alt="Caro Adamska"
-            className="float-right rounded-lg overflow-hidden"
+            className="rounded-lg overflow-hidden text-center"
           />
-          <div className="social-media">
-            <ul>
-              <li>icon karolina.g.adamska@gmail.com</li>
-              <li>+48 510 593 718</li>
-              <li>instagram.com/karo</li>
-              <li>caro adamska stylist</li>
-            </ul>
+          <div className="flex flex-wrap justify-center md:justify-start">
+            <div className="">
+              <div className="mt-6 px-4 mb-6 md:mb-0">
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center">
+                    <Image
+                      src={"/envelope.png"}
+                      width={16}
+                      height={16}
+                      alt="email"
+                      className="mr-2"
+                    />
+                    <p className="text-16">karolina.g.adamska@gmail.com</p>
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      src={"/phone.png"}
+                      width={16}
+                      height={16}
+                      alt="phone"
+                      className="mr-2"
+                    />
+                    <p className="text-16">+48 510 593 718</p>
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      src={"/instagram.png"}
+                      width={16}
+                      height={16}
+                      alt="instagram"
+                      className="mr-2"
+                    />
+                    <p className="text-16">instagram.com/caroadamska</p>
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      src={"/messanger.png"}
+                      width={16}
+                      height={16}
+                      alt="messanger"
+                      className="mr-2"
+                    />
+                    <p className="text-16">caroadamskastylist</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="w-full sm:w-2/3">2/3 width</div>
+        <div className="flex w-full sm:w-2/3 md:pl-12 px-8 md:px-0">
+          {/* <div className="w-full"> */}
+          <ContactForm />
+          {/* </div> */}
+        </div>
       </div>
     </div>
   );
---
+++ components/contactForm/ContactForm.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useState } from "react";
+
+interface FormData {
+  name: string;
+  company: string;
+  email: string;
+  message: string;
+}
+
+const ContactForm: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    company: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        alert("Message sent successfully!");
+        setFormData({ name: "", company: "", email: "", message: "" });
+      } else {
+        alert("Failed to send message");
+      }
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="name"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full border border-black rounded-md px-3 py-2 focus:outline-none focus:border-salmon focus:ring-1 focus:ring-salmon"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="company name"
+            id="company"
+            name="company"
+            value={formData.company}
+            onChange={handleChange}
+            className="w-full border border-black rounded-md px-3 py-2 focus:outline-none focus:border-salmon focus:ring-1 focus:ring-salmon"
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="e-mail address"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full border px-3 py-2 border-black rounded-md focus:outline-none focus:border-salmon focus:ring-1 focus:ring-salmon"
+          />
+        </div>
+        <div className="mb-4">
+          <textarea
+            id="message"
+            name="message"
+            placeholder="message"
+            value={formData.message}
+            onChange={handleChange}
+            className="w-full h-32 md:h-64 rounded-md px-3 py-2 border border-black 
+      focus:outline-none focus:border-salmon focus:ring-1 focus:ring-salmon
+      "
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="w-full md:w-auto bg-salmon/40 text-black border border-black px-12 py-2 rounded-md hover:bg-salmon float-right"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
