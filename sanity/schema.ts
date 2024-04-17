3530b1d48be2b9f44fa5482dc05c2b300c4d0c36
boguslawdanek <boguslawdanek@gmail.com>
2024-04-17 13:27:01 +0200

    schemas updated

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -5,7 +5,8 @@ import personalProjects from './schemas/personalProjects'
 import footer from './schemas/footer'
 import contact from './schemas/contact'
 import reviews from './schemas/reviews'
+import publications from './schemas/publications'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [about, introduction, personalProjects, footer, contact, reviews],
+  types: [about, introduction, personalProjects, footer, contact, reviews, publications],
 }
