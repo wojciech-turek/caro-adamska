9b41e7c491cb7178701100f47ad92915a81e0bd5
boguslawdanek <boguslawdanek@gmail.com>
2024-04-17 13:34:44 +0200

    sanity - publications

--- app/publications/[publicationsId]/page.tsx
+++ app/publications/[publicationsId]/page.tsx
@@ -1,7 +1,63 @@
-export default function PublicationsDetails({ params }) {
+import { client, urlFor } from "@/app/lib/sanity";
+import Image from "next/image";
+
+interface PublicationsDetailsProps {
+  params: {
+    publicationsId: string;
+  };
+}
+
+interface SlugInterface {
+  current: string;
+  _type: string;
+}
+
+interface FilteredDataData {
+  gallery: any;
+  slug: SlugInterface;
+}
+
+export default async function PublicationsDetails({
+  params,
+}: PublicationsDetailsProps) {
+  async function getData() {
+    const query = `
+    *[_type == 'publications'] {
+      gallery, slug
+    }
+    `;
+
+    const data = await client.fetch(query);
+    return data;
+  }
+
+  const data: FilteredDataData[] = await getData();
+
+  const currentPublicationImages: FilteredDataData[] = data.filter(
+    (item) => item.slug.current === params.publicationsId
+  );
+  console.log("filteredData,", currentPublicationImages[0]);
+
+  interface CurrentPublicationImages {
+    image: string;
+  }
+  const gallery: CurrentPublicationImages[] =
+    currentPublicationImages[0].gallery;
+
   return (
-    <div className="w-full text-center mb-114">
-      <h1>Publications details {params.publicationsId}</h1>
-    </div>
+    <>
+      <div className="container mx-auto">
+        {gallery.map((image, index) => (
+          <Image
+            key={index}
+            className="rounded-lg mx-auto"
+            width={395}
+            height={395}
+            src={urlFor(image).url()}
+            alt={"/"}
+          />
+        ))}
+      </div>
+    </>
   );
 }
--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -1,7 +1,76 @@
-export default function Publications() {
+import { client, urlFor } from "@/app/lib/sanity";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getData() {
+  const query = `
+  *[_type == 'publications'] {
+    image, slug, _id
+  }
+  `;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+interface HomePagePublicationsProps {
+  title: string;
+  url?: string;
+}
+
+interface SlugInterface {
+  current: string;
+  _type: string;
+}
+
+interface PublicationsData {
+  image: string;
+  slug: SlugInterface;
+  _id: string;
+}
+
+export default async function Publications({
+  title,
+}: HomePagePublicationsProps) {
+  const data: PublicationsData[] = await getData();
+
   return (
-    <div className="w-full text-center mb-114">
-      <h1>Publications</h1>
+    <div className="container mx-auto">
+      <h3 className="text-4xl mb-8 font-medium text-center">{title}</h3>
+
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full mb-8 md:mb-100"
+      >
+        <CarouselContent>
+          {data.map((item, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+              <Link href={`/publications/${item.slug.current}`}>
+                <>
+                  <Image
+                    className="rounded-lg mx-auto"
+                    width={395}
+                    height={395}
+                    src={urlFor(item.image).url()}
+                    alt={"1"}
+                  />
+                </>
+              </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
