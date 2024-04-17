e14adb89f8e38b6ea1ad03ca1fad7a184ce49b55
boguslawdanek <boguslawdanek@gmail.com>
2024-04-17 14:49:29 +0200

    Home Page updated

--- app/commecial-collabs/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function CommercialCollabs() {
-  return (
-    <div className="w-full text-center mb-114">
-      <h1>commercial collbs</h1>
-    </div>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,16 @@ import Contact from "@/components/contact/contact";
 import Introduction from "@/components/introduction/introduction";
 import Reviews from "@/components/reviews/reviews";
 import Publications from "./publications/page";
+import PersonalProjects from "./personal-projects/page";
+import CommercialCollabs from "./commercial-collabs/page";
 
 export default function Home() {
   return (
     <>
       <Introduction />
       <Publications title={"Publications"} />
-      <MainPageCarousel
-        title={"Commercial collabs"}
-        url={"/commecial-collabs"}
-      />
-      <MainPageCarousel
-        title={"Personal projects"}
-        url={"/personal-projects"}
-      />
+      <CommercialCollabs title={"Commercial collabs"} />
+      <PersonalProjects title="Personal projects" />
       <Reviews />
       <Contact />
     </>
