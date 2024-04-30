439613ed290d60264321a7f2b3c3cc65f4d8b69c
boguslawdanek <boguslawdanek@gmail.com>
2024-04-30 16:10:37 +0200

    Correct RWD for commercial collabs and personal-projects page

--- app/personal-projects/[projectId]/page.tsx
+++ app/personal-projects/[projectId]/page.tsx
@@ -44,21 +44,19 @@ export default async function PersonalProjectsDetails({
     currentPersonalProjects[0].gallery;
 
   return (
-    <>
+    <div className="flex flex-row flex-wrap">
       {gallery.map((image, index) => (
-        <div key={index} className="mx-auto">
-          <div className="md:basis-1/2 lg:basis-1/3">
-            <Image
-              key={index}
-              className="rounded-lg h-480 py-2 md:py-0"
-              width={395}
-              height={395}
-              src={urlFor(image).url()}
-              alt={"/"}
-            />
-          </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 py-2 px-1">
+          <Image
+            key={index}
+            className="rounded-lg h-480 py-2 md:py-0"
+            width={395}
+            height={395}
+            src={urlFor(image).url()}
+            alt={"/"}
+          />
         </div>
       ))}
-    </>
+    </div>
   );
 }
