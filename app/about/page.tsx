991ba409407a8251c4776426b951550b7b732697
boguslawdanek <boguslawdanek@gmail.com>
2024-04-30 14:50:01 +0200

    About page - image ratio fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -28,17 +28,19 @@ export default async function About() {
         <h1 className="text-4xl mb-6 md:mb-12 lg:mb-24 font-medium">{title}</h1>
       </div>
       <div className="flex flex-col lg:flex-row">
-        <div className="flex lg:w-1/2">
-          <Image
-            width={572}
-            height={564}
-            src={urlFor(image).url()}
-            alt={title}
-            className="rounded-lg overflow-hidden mb-8 block mx-auto justify-center"
-          />
+        <div className="flex lg:w-1/2 justify-center align-middle">
+          <div>
+            <Image
+              width={572}
+              height={564}
+              src={urlFor(image).url()}
+              alt={title}
+              className="rounded-lg overflow-hidden mb-8 block mx-auto justify-center w-[250px] lg:w-[1200px] lg:h-auto"
+            />
+          </div>
         </div>
-        <div className="flex w-full lg:w-1/2 md:pl-4">
-          <div className="prose prose-p:text-lg prose-p:mb-4 prose-p:mt-0">
+        <div className="flex w-full lg:w-1/2 md:pl-4 lg:ml-8">
+          <div className="prose prose-p:text-lg prose-p:mb-4 prose-p:mt-0 justify-center">
             <PortableText value={content} />
           </div>
         </div>
